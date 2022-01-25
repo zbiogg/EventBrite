fee623be2b0084e2830d8bb18719cf4ab52eb585
Tùng Zbio <tuikhongnhotk@gmail.com>
2022-01-25 13:35:17 +0700

    gnut update css by module

--- src/components/Home/Browser/index.js
+++ src/components/Home/Browser/index.js
@@ -1,76 +1,52 @@
-import './Brower.css'
-
+import clsx  from 'clsx';
+import style from './Browser.module.css'
+import iconHeart from '../../../assets/icons/heart.png'
+import iconUser from '../../../assets/icons/user.png'
+import iconLocation from '../../../assets/icons/location.png'
 function Browser() {
     return (
-        <div className=" wrap-container-search-event">
-            <div className="wrap-list-category">
-                <ul className="list-category">
-                    <li className="category-item">
-                        <span className="category-name">All</span>
+        <div className={style.wrapContainerSearchEvent}>
+            <div className={style.wrapListCategory}>
+                <ul className={style.listCategory}>
+                    <li className={style.categoryItem}>
+                        <span className={style.categoryName}>All</span>
                     </li>
-                    <li className="category-item">
-                        <span className="category-name">Game</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Paint</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Car</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Sport</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Travel</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Food</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Animal</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Pet</span>
-                    </li>
-                    <li className="category-item">
-                        <span className="category-name">Others</span>
-                    </li>
-
-
                 </ul>
             </div>
-            <div className="result-list-event">
-                <div className="result-event-location">
-                    <h4 className="title-result-event">MEETING IN LOCATION CITY</h4>
-                    <div className="wrap-list-event row">
-                        <div className="wrap-item-event col-sm-6 col-lg-4 col-xl-3">
-                            <div className="item-event">
-                                <div className="wrap-image-event">
-                                    <img className="img-event"
+            <div className={style.resultListEvent}>
+                <div className={style.resultEventLocation}>
+                    <h4 className={style.titleResultEvent}>MEETING IN LOCATION CITY</h4>
+                    <div className={clsx(style.wrapListEvent, 'row')}>
+                        {/* "wrap-item-event col-sm-6 col-lg-4 col-xl-3 */}
+                        <div className={clsx(style.wrapItemEvent, 'col-sm-6 col-lg-4 col-xl-3')}>
+                            <div className={style.itemEvent}>
+                                <div className={style.wrapImageEvent}>
+                                    <img className={style.imgEvent}
                                         src="https://firebasestorage.googleapis.com/v0/b/yamemoim-76929.appspot.com/o/event%2F2320917954_gnut%2Fbackground%2Fbg.png?alt=media&token=7e1c0320-969e-4897-a88d-fea673c1d797"
                                          />
-                                    <span className="wrap-button-favorite">
-                                        <img src="/img/icons/heart.png" width="24px" height="24px"  />
+                                    <span className={style.wrapButtonFavorite}>
+                                        <img src={iconHeart} width="24px" height="24px"  />
                                     </span>
                                 </div>
-                                <div className="wrap-event-info">
-                                    <div className="wrap-ctg-of-event">
+                                <div className={style.wrapEventInfo}>
 
-                                        <img className="img-ctg-of-event"
+                                    <div className={style.wrapCtgOfEvent}>
+
+                                        <img className={style.imgCtgOfEvent}
                                             src="https://firebasestorage.googleapis.com/v0/b/yamemoim-76929.appspot.com/o/category%2F1.png?alt=media&token=9a4aa03d-648a-479d-803a-1bf3cda7cd08"
                                              width="100%" height="100%" />
 
-                                        <span className="event-ctg-name">
+                                        <span className={style.eventCtgName}>
                                             Game
                                         </span>
                                     </div>
-                                    <h5 className="event-name">VCS Championship 2022</h5>
-                                    <div className="wrap-event-member">
-                                        <img src="/img/icons/user.png" width="16px" height="16px"  />
+                                    <h5 className={style.eventName}>VCS Championship 2022</h5>
+                                    <div className={style.wrapEventMember}>
+                                        <img src={iconUser} width="16px" height="16px"  />
                                         <span>89 người</span>
                                     </div>
-                                    <div className="wrap-event-location">
-                                        <img src="/img/icons/location.png" width="16px" height="16px"  />
+                                    <div className={style.wrapEventLocation}>
+                                        <img src={iconLocation} width="16px" height="16px"  />
                                         <span>Quận Thanh Xuân</span>
                                     </div>
                                 </div>
@@ -78,53 +54,55 @@ function Browser() {
                         </div>
                        
                     </div>
-                    <div className="wrap-btn-see-more">
-                        <button className="btn btn-see-more">See more</button>
+                    <div className={style.wrapBtnSeeMore}>
+                        <button className={style.btnSeeMore}>See more</button>
                     </div>
                 </div>
 
-                <div className="hr-custom"></div>
+                <div className={style.hrCustom}></div>
 
-                <div className="result-event-nationwide">
-                    <h4 className="title-result-event">MEETING IN NATIONWIDE</h4>
-                    <div className="wrap-list-event row">
-                        <div className="wrap-item-event col-sm-6 col-lg-4 col-xl-3">
-                            <div className="item-event">
-                                <div className="wrap-image-event">
-                                    <img className="img-event"
+                <div className={style.resulEventMationwide}>
+                <h4 className={style.titleResultEvent}>MEETING IN NATIONWIDE</h4>
+                    <div className={clsx(style.wrapListEvent, 'row')}>
+                        {/* "wrap-item-event col-sm-6 col-lg-4 col-xl-3 */}
+                        <div className={clsx(style.wrapItemEvent, 'col-sm-6 col-lg-4 col-xl-3')}>
+                            <div className={style.itemEvent}>
+                                <div className={style.wrapImageEvent}>
+                                    <img className={style.imgEvent}
                                         src="https://firebasestorage.googleapis.com/v0/b/yamemoim-76929.appspot.com/o/event%2F2320917954_gnut%2Fbackground%2Fbg.png?alt=media&token=7e1c0320-969e-4897-a88d-fea673c1d797"
                                          />
-                                    <span className="wrap-button-favorite">
-                                        <img src="/img/icons/heart.png" width="24px" height="24px"  />
+                                    <span className={style.wrapButtonFavorite}>
+                                        <img src={iconHeart} width="24px" height="24px"  />
                                     </span>
                                 </div>
-                                <div className="wrap-event-info">
-                                    <div className="wrap-ctg-of-event">
+                                <div className={style.wrapEventInfo}>
 
-                                        <img className="img-ctg-of-event"
+                                    <div className={style.wrapCtgOfEvent}>
+
+                                        <img className={style.imgCtgOfEvent}
                                             src="https://firebasestorage.googleapis.com/v0/b/yamemoim-76929.appspot.com/o/category%2F1.png?alt=media&token=9a4aa03d-648a-479d-803a-1bf3cda7cd08"
                                              width="100%" height="100%" />
 
-                                        <span className="event-ctg-name">
+                                        <span className={style.eventCtgName}>
                                             Game
                                         </span>
                                     </div>
-                                    <h5 className="event-name">VCS Championship 2022</h5>
-                                    <div className="wrap-event-member">
-                                        <img src="/img/icons/user.png" width="16px" height="16px"  />
+                                    <h5 className={style.eventName}>VCS Championship 2022</h5>
+                                    <div className={style.wrapEventMember}>
+                                        <img src={iconUser} width="16px" height="16px"  />
                                         <span>89 người</span>
                                     </div>
-                                    <div className="wrap-event-location">
-                                        <img src="/img/icons/location.png" width="16px" height="16px"  />
+                                    <div className={style.wrapEventLocation}>
+                                        <img src={iconLocation} width="16px" height="16px"  />
                                         <span>Quận Thanh Xuân</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+                       
                     </div>
-                    <div className="wrap-btn-see-more">
-                        <button className="btn btn-see-more">See more</button>
+                    <div className={style.wrapBtnSeeMore}>
+                        <button className={style.btnSeeMore}>See more</button>
                     </div>
                 </div>
 
