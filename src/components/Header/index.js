af02d605f1ecdd3be838fbb66339b36b1e62dd52
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2022-01-26 09:06:00 +0700

    NagaKi convert to header module css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/role-supports-aria-props */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-
-import  './Header.css'
+import clsx from 'clsx';
+import style from './Header.module.css'
 import more from '../../assets/icons/more.png'
 import search from '../../assets/icons/search.png'
 import logo from '../../assets/logo.png'
@@ -9,6 +9,7 @@ import right_arrow from '../../assets/icons/right-arrow.png'
 import trending from '../../assets/icons/trending.png'
 import close from '../../assets/icons/close.png'
 import announcement from '../../assets/icons/announcement.png'
+
 
 function showSearch(){
     var x = document.getElementById("search-box");
@@ -23,114 +24,109 @@ function searchBoxClose(){
 
 function Header() {
     return (
-        <div>
-            <div className="search-box" id="search-box">
+<div>
+    <div className={style.searchBox} id="search-box">
 
-<div className="container-fluid d-flex justify-content-end">
-    <span><img src={close} width="30px" onClick={searchBoxClose} role="button"  alt =""/></span>
-</div>
-
-<div className="row">
-    <div className="col-md-6">
-        <form className="search-input">
-            <span><img className="search-box-icon" src={search} alt=""/></span>
-            <input className="search-input-text" type="text" placeholder="Search for anything"/>
-            <span><img className="search-box-icon" src={right_arrow} alt="" /></span>
-        </form>
-    </div>
-    <div className="col-md-6 suggest-div">
-        <div className="list">
-            <ul className="suggest-list">
-                <li className="suggest-item">
-                    <div className="suggest-title">
-                        <span><img src={announcement} width="15px" alt="" /></span>
-                        <span className="suggest">Suggested</span> 
-                    </div>
-                </li>
-                <li className="suggest-item"><a href="#">Online</a></li>
-                <li className="suggest-item"><a href="#">Today</a></li>
-                <li className="suggest-item"><a href="#">This Week</a></li>
-                <li className="suggest-item"><a href="#">This month</a></li>
-                <li className="suggest-item"><a href="#">Food & drink</a></li>
-                <li className="suggest-item"><a href="#">Farm</a></li>
-                <li className="suggest-item"><a href="#">Pet</a></li>
-                <li className="suggest-item"><a href="#">Car & man</a></li>
-                <li className="suggest-item"><a href="#">Game</a></li>
-                <li className="suggest-item">
-                    <div className="suggest-title">
-                        <span><img src={trending} alt="" width="15px" style={{marginBottom: '1%'}}/></span>
-                        <span className="suggest">Trending</span> 
-                    </div>
-                </li>
-                <li className="suggest-item"><a href="#">Bitcoin</a></li>
-                <li className="suggest-item"><a href="#">Dance</a></li>
-                <li className="suggest-item"><a href="#">Geting over it</a></li>
-                <li className="suggest-item"><a href="#">Anime</a></li>
-                <li className="suggest-item"><a href="#">Genshin impact</a></li>
-                <li className="suggest-item"><a href="#">Honkai impact</a></li>
-            </ul>
+        <div className={clsx('container-fluid','d-flex', 'justify-content-end')}>
+            <span><img src={close} width="30px" onClick={searchBoxClose} role="button"  alt =""/></span>
         </div>
-    </div>
-</div>
 
-</div>
-<div className="container-fluid header">
-<div className="row">
-    <div className="col">
-        <div className="row left">
-            <span className="col-md-3 text-center align-middle"><a className="nav-logo" href="#"><img className="logo-header" src={logo} alt=""/></a></span>
-            <div className="col" >
-                <div className="search-nav" onClick={showSearch} role="button">
-                    <span><img className="search-icon" src={search} alt=""/></span>
-                    <span>Click to search</span>
+        <div className={clsx('row')}>
+            <div className={clsx("col-md-6")}>
+                <form className={style.searchInput}>
+                    <span><img className={style.searchBoxIcon} src={search} alt=""/></span>
+                    <input className={style.searchInputText} type="text" placeholder="Search for anything"/>
+                    <span><img className={style.searchBoxIcon} src={right_arrow} alt="" /></span>
+                </form>
+            </div>
+            <div className={clsx('col-md-6', 'suggest-div')}>
+                <div className={style.list}>
+                    <ul className={style.suggestList}>
+                        <li className={style.suggestItem}>
+                            <div className={style.suggestTitle}>
+                                <span><img src={announcement} width="15px" alt="" /></span>
+                                <span className={style.suggest}>Suggested</span> 
+                            </div>
+                        </li>
+                        <li className={style.suggestItem}><a href="#">Online</a></li>
+                        <li className={style.suggestItem}><a href="#">Today</a></li>
+                        <li className={style.suggestItem}><a href="#">This Week</a></li>
+                        <li className={style.suggestItem}><a href="#">This month</a></li>
+                        <li className={style.suggestItem}><a href="#">Food & drink</a></li>
+                        <li className={style.suggestItem}><a href="#">Farm</a></li>
+                        <li className={style.suggestItem}><a href="#">Pet</a></li>
+                        <li className={style.suggestItem}><a href="#">Car & man</a></li>
+                        <li className={style.suggestItem}><a href="#">Game</a></li>
+                        <li className={style.suggestItem}>
+                            <div className={style.suggestTitle}>
+                                <span><img src={trending} alt="" width="15px" style={{marginBottom: '1%'}}/></span>
+                                <span className={style.suggest}>Trending</span> 
+                            </div>
+                        </li>
+                        <li className={style.suggestItem}><a href="#">Bitcoin</a></li>
+                        <li className={style.suggestItem}><a href="#">Dance</a></li>
+                        <li className={style.suggestItem}><a href="#">Geting over it</a></li>
+                        <li className={style.suggestItem}><a href="#">Anime</a></li>
+                        <li className={style.suggestItem}><a href="#">Genshin impact</a></li>
+                        <li className={style.suggestItem}><a href="#">Honkai impact</a></li>
+                    </ul>
                 </div>
             </div>
-        </div>  
-    </div>
-    <div className="col">
-        <ul className="header-nav justify-content-end">
-            <li className="item dropdown web">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Organize
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Why Yamemoim?</a>
-                    <a className="dropdown-item" href="#">Pricing</a>
-                </div>
-            </li>
-            <li className="item dropdown web">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                   Help
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                </div>
-            </li>
-            <li className="item mobile"><a><span><img role="button" onClick={showSearch} src={search} alt="" width="20px"/></span></a></li>
-            <li className="item dropdown mobile">
-                <a className="nav-link" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                   <span><img src={more} style={{width: 20}} alt=""/></span>
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Organize</a>
-                    <a className="dropdown-item" href="#">Help</a>
-                    <a className="dropdown-item create-event" href="#">Create an event</a>
-                </div>
-            </li>
-            <li className="item create-event web"><a href="#" className="nav-link" role="button">Create an event</a></li>
-            <li className="item"><a className="nav-link" role="button" href="#">Sign in</a></li>    
-        </ul>
-    </div>
-</div>
-</div>
-
-
-
-
-
-
         </div>
+
+    </div>
+    <div className={clsx('container-fluid', style.header)}>
+        <div className={clsx('row')}>
+            <div className={clsx('col')}>
+                <div className={clsx('row')}>
+                    <span className={clsx('col-md-3', 'text-center', 'align-middile')}><a className={style.NavLogo} href="#"><img className={style.logoHeader} src={logo} alt=""/></a></span>
+                    <div className={clsx('col')} >
+                        <div className={style.searchNav} onClick={showSearch} role="button">
+                            <span><img className={style.searchIcon} src={search} alt=""/></span>
+                            <span>Click to search</span>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+            <div className={clsx('col')}>
+                <ul className={clsx(style.headerNav, 'justify-content-end')}>
+                    <li className={clsx(style.item, 'dropdown',style.web)}>
+                        <a className={clsx('nav-link', 'dropdown-toggle')} href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Organize
+                        </a>
+                        <div className={clsx('dropdown-menu')} aria-labelledby="navbarDropdown">
+                            <a className={clsx('dropdown-item')} href="#">Why Yamemoim?</a>
+                            <a className={clsx('dropdown-item')} href="#">Pricing</a>
+                        </div>
+                    </li>
+                    <li className={clsx(style.item, 'dropdown',style.web)}>
+                        <a className={clsx('nav-link', 'dropdown-toggle')} href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Help
+                        </a>
+                        <div className={clsx('dropdown-menu')} aria-labelledby="navbarDropdown">
+                            <a className={clsx('dropdown-item')} href="#">Action</a>
+                            <a className={clsx('dropdown-item')} href="#">Another action</a>
+                        </div>
+                    </li>
+                    <li className={clsx(style.item, style.mobile)}><a><span><img role="button" onClick={showSearch} src={search} alt="" width="20px"/></span></a></li>
+                    <li className={clsx(style.item, 'dropdown', style.mobile)}>
+                        <a className={clsx('nav-link')} href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span><img src={more} style={{width: 20}} alt=""/></span>
+                        </a>
+                        <div className={clsx('dropdown-menu')} aria-labelledby="navbarDropdown">
+                            <a className={clsx('dropdown-item')} href="#">Organize</a>
+                            <a className={clsx('dropdown-item')} href="#">Help</a>
+                            <a className={clsx('dropdown-item', style.createEvent)} href="#">Create an event</a>
+                        </div>
+                    </li>
+                    <li className={clsx(style.item, style.createEvent, style.web)}><a href="#" className={clsx('nav-link')} role="button">Create an event</a></li>
+                    <li className={style.item}><a className={clsx('nav-link')} role="button" href="#">Sign in</a></li>    
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
     );
 }
 
