aa4a8998e7f8d3f88d2e619cafd936a1c37da8d8
nvsang260720 <nvsang.18it2@vku.udn.vn>
2022-01-28 10:47:28 +0700

    commit validate login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,41 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import clsx from 'clsx';
 import { useState,useEffect} from 'react';
+import {useFormik} from 'formik'
+import * as Yup from "yup";
 import Footer from '../../components/Footer/index'
 import {database} from '../../lib/firebase'
 import logo from '../../assets/logo.png'
 import background from '../../assets/img/backgroud.jpg'
+import './login.css'
 export default function Login (){
     const [userName , setUsername] = useState()
     const [passWord, setPassWord] = useState()
+
+    const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+      phone: "",
+      password: "",
+      confirmedPassword: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Required")
+        .min(4, "Must be 4 characters or more"),
+      password: Yup.string()
+        .required("Required")
+        .matches(
+          /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,19}$/,
+          "Password must be 7-19 characters and contain at least one letter, one number and a special character"
+        ),
+    }),
+    onSubmit: (values) => {
+      window.alert("Form submitted");
+      console.log(values);
+    },
+  });
 
     useEffect(() => {
         function getLogin(){
@@ -25,60 +53,47 @@ export default function Login (){
             })
         }
         getLogin()
-        console.log(userName)
-        console.log(passWord)
 
     })
     return(
-    <div>
+    <>
         <div className={clsx('container-fluid')}>
-            
-            <div className="row">
+        <div className="row">
                 <div className="col-sm-12 col-md-6" >
-                    <div style={{ justifyContent: "center" ,display: "flex" }}>
-                        <form style={{ margin:20, width:'50%'}}>
+                    <div style={{ justifyContent: "center" ,display: "flex"}}>
+                        <form className="infoform" onSubmit={formik.handleSubmit}>
                             <a href='/'><img src={logo} style={{ width: 150, marginBottom:20}} alt=""/> </a>
                             <h1 >Log in</h1>
-                            <div className="form-group" >
-                                <label>Email</label>
-                                <input 
-                                    type="email" 
-                                    className="form-control" 
-                                    placeholder="Enter email" 
-                                    style={{
-                                        width: "100%",
-                                        paddingLeft: "8px",
-                                        paddingTop: "6px",
-                                        paddingBottom: "6px",
-                                    }}
-                                    onChange={event => setUsername(event.target.value)} 
-
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Password</label>
-                                <input 
-                                    type="password" 
-                                    className="form-control" 
-                                    placeholder="Enter password" 
-                                    onChange={event => setPassWord(event.target.value)} 
-
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <div className="custom-control custom-checkbox">
-                                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                                </div>
-                            </div>
-
+                            <label> Your name </label>
+                            <input
+                                style={{width: 400}}
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                placeholder="Enter your name"
+                            />
+                            {formik.errors.name && (
+                            <p className="errorMsg"> {formik.errors.name} </p>
+                            )}
+                            <label> Password </label>
+                            <input
+                                type="text"
+                                id="password"
+                                name="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                placeholder="Enter your password"
+                            />
+                            {formik.errors.password && (
+                            <p className="errorMsg"> {formik.errors.password} </p>
+                            )}
                             <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>         
                             <p className="forgot-password text-right">
                                 Forgot <a href="#">password?</a>
                             </p>
-                        </form>   
+                        </form>
                     </div>
                     
                 </div>
@@ -86,11 +101,9 @@ export default function Login (){
                     
                 </div>
             </div>
-            
+        
         </div>
         <Footer/>  
-    </div>
-   
-    
+    </>
     )
 }
